ae2cc161b7a51223996a452bdafec3cd4d8c74e0
bodakarenyjere <boda.karen.jere@gmail.com>
2024-08-23 14:42:03 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -112,23 +112,6 @@
             }
         }
     });
-
-const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
-
-let countDown = new Date('Oct 18, 2024 00:00:00').getTime(),
-    x = setInterval(function() {
-
-      let now = new Date().getTime(),
-          distance = countDown - now;
-
-      document.getElementById('dias').innerText = Math.floor(distance / (day)),
-        document.getElementById('horas').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutos').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('segundos').innerText = Math.floor((distance % (minute)) / second);
-
-    }, second)
+    
 })(jQuery);
 
